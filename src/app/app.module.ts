c4dc423c820d66264d2d3281077a001f9a9d5d81
Woody3097 <nicolay.danch@gmail.com>
2021-12-13 00:01:00 +0200

    Added routes, created header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,43 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { StudentComponent } from './core/student/student.component';
+import { TeacherComponent } from './core/teacher/teacher.component';
+import { ScheduleComponent } from './core/schedule/schedule.component';
+import {RouterModule} from "@angular/router";
+
+const routes = [
+  {
+    path: 'main',
+    component: ScheduleComponent,
+  },
+  {
+    path: 'student',
+    component: StudentComponent,
+  },
+  {
+    path: 'teacher',
+    component: TeacherComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'main',
+    pathMatch: 'full',
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    StudentComponent,
+    TeacherComponent,
+    ScheduleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
